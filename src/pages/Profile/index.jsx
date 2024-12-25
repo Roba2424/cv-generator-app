08cede281b9e413993398aed16281d151f402733
Robert Davidyan <robert.davidyan@Roberts-MacBook-Pro.local>
2024-12-25 21:00:04 +0400

    style: add new profile styling

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,21 +1,23 @@
 import { Button } from "antd";
 import { Link } from "react-router-dom";
 import { ROUTE_CONSTANTS } from "../../utils/constant";
+import "./style.css";
 
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <div>
-        <h2>My Resumes</h2>
-        <Button>
+    <div className="profile-container">
+      <h1 className="profile-heading">Profile Page</h1>
+
+      <div className="profile-section">
+        <h2 className="profile-subheading">My Resumes</h2>
+        <Button type="primary" className="profile-button">
           <Link to={ROUTE_CONSTANTS.RESUMES}>Resumes</Link>
         </Button>
       </div>
 
-      <div>
-        <h2>Create Resume</h2>
-        <Button>
+      <div className="profile-section">
+        <h2 className="profile-subheading">Create Resume</h2>
+        <Button type="dashed" className="profile-button">
           <Link to={ROUTE_CONSTANTS.RESUME_BUILDER}>Create Resume</Link>
         </Button>
       </div>
